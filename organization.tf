c579a6ad42a9917940aca636d9a6047c4dd9e017
Aleksa Siriški <139220691+cyberaleksa@users.noreply.github.com>
2023-10-13 14:26:36 +0200

    reverted org.tf

--- organization.tf
+++ organization.tf
@@ -1,7 +1,13 @@
-resource "aws_organizations_organization" "organization" {
-  feature_set                   = local.config.feature_set
-  enabled_policy_types          = local.config.enabled_policy_types
+locals {
+  feature_set = local.config.feature_set
+  enabled_policy_types = local.config.enabled_policy_types
   aws_service_access_principals = local.config.aws_service_access_principals
+}
+
+resource "aws_organizations_organization" "organization" {
+  feature_set                   = local.feature_set
+  enabled_policy_types          = local.enabled_policy_types
+  aws_service_access_principals = local.aws_service_access_principals
 
   lifecycle {
     prevent_destroy = true
